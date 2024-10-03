4e648fe186491b97ee7c109c2ff9b45c1d5442af
CadenLS <csinclair@student.neumont.edu>
2024-10-03 16:05:53 -0600

    Movement
    Now has basic movement and jump

---
+++ Assets/Scripts/Player Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    private Rigidbody2D rb; // Rigidbody2D component reference
+    private Vector2 movement; // Store input
+
+    // Basic Variables
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.1f;
+
+    private bool isOnGround;
+
+
+    private void Start()
+    {
+        // Gets component in case not there
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Gets input from Input Manager
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+        isOnGround = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckRadius, LayerMask.GetMask("Ground"));
+
+        //Debug.Log("Is on ground: " + isOnGround);
+
+        if (Input.GetButtonDown("Jump") && isOnGround)
+        {
+            rb.AddForce(new Vector2(rb.linearVelocityX, jumpForce));
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.position = rb.position + movement * moveSpeed * Time.fixedDeltaTime;
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Set gizmo color to blue
+        Gizmos.color = Color.blue;
+
+        // Draw a wire sphere at the groundCheck position to represent the ground check radius
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+
+}
