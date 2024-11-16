424a7cc75400ab6fcf4fa80a0be3872fecbdca8a
CadenLS <csinclair@student.neumont.edu>
2024-11-15 18:04:09 -0700

    Better Camera
    Camera now looks more fluid and is customizable.

---
+++ Assets/Scripts/Random/CameraController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Vector3 targetPoint = Vector3.zero;
+    public PlayerMovement player;
+
+    public float moveSpeed;
+    public float lookAheadDistance = 5f, lookAheadSpeed = 3f;
+    private float lookOffset;
+
+    private bool isFalling;
+    public float maxVertOffset = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        targetPoint = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+
+    void LateUpdate()
+    {
+        if (player.isOnGround)
+        {
+            targetPoint.y = player.transform.position.y;
+        }
+
+        if (transform.position.y - player.transform.position.y > maxVertOffset)
+        {
+            isFalling = true;
+        }
+
+        if (isFalling)
+        {
+            targetPoint.y = player.transform.position.y;
+
+            if (player.isOnGround)
+            {
+                isFalling = false;
+            }
+        }
+
+        if (player.rb.linearVelocity.x > 0f)
+        {
+            lookOffset = Mathf.Lerp(lookOffset, lookAheadDistance, lookAheadSpeed * Time.deltaTime);
+        }
+        if (player.rb.linearVelocity.x < 0f)
+        {
+            lookOffset = Mathf.Lerp(lookOffset, -lookAheadDistance, lookAheadSpeed * Time.deltaTime);
+        }
+
+        targetPoint.x = player.transform.position.x + lookOffset;
+
+        transform.position = Vector3.Lerp(transform.position, targetPoint, moveSpeed * Time.deltaTime);
+
+    }
+
+}
